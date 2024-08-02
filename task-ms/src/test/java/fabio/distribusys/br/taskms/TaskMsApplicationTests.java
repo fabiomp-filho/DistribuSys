b3a1215b71438bec64943b992d38a6819cfc028e
Fábio Martins <fabio.filho0820@gmail.com>
2024-08-02 18:06:11 -0300

    Creation of task microservice

---
+++ task-ms/src/test/java/fabio/distribusys/br/taskms/TaskMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fabio.distribusys.br.taskms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
