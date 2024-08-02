0f9015bb85e4c804177c2441a2f4408f3d18a5e4
Fábio Martins <fabio.filho0820@gmail.com>
2024-08-02 01:31:42 -0300

    feat: create users

---
+++ user-ms/src/main/java/fabio/distribusys/br/userms/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package fabio.distribusys.br.userms.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({BusinessException.class})
+    public ResponseEntity<String> handleBusinessException(BusinessException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno do servidor: " + e.getClass());
+    }
+
+
+}
---
+++ user-ms/src/main/java/fabio/distribusys/br/userms/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package fabio.distribusys.br.userms.mapper;
+
+import fabio.distribusys.br.userms.domain.dto.UserRequestDTO;
+import fabio.distribusys.br.userms.domain.dto.UserResponseDTO;
+import fabio.distribusys.br.userms.domain.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User toEntity(UserRequestDTO userRequest);
+
+    UserResponseDTO toDTO(User user);
+}
